7ff8b4d200e6189a7c8adb2c2352f4e75b57069e
Steve Hairston <92773022+stevehairston@users.noreply.github.com>
2024-03-21 06:52:30 -0400

    restore sidebar component

---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import profileAvatar from "../../images/avatar.svg";
+import "../SideBar/SideBar.css";
+
+const SideBar = ({ profileName }) => {
+  return (
+    <div className="sidebar">
+      <div className="sideBar__info">
+        <img
+          src={profileAvatar}
+          className="Profile__image"
+          alt="Profile-avatar"
+        />
+        <p className="profile__name">{profileName}</p>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
